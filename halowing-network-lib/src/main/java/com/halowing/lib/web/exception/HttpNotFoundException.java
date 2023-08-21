04aca3f1b12055f931c413d4ed255d73b90f7a1a
sgkim <sgkim@halowing.com>
2023-08-22 02:44:13 +0900

    httpStatus 404 처리용 Exception 작성.

--- halowing-network-lib/src/main/java/com/halowing/lib/web/exception/HttpNotFoundException.java
+++ halowing-network-lib/src/main/java/com/halowing/lib/web/exception/HttpNotFoundException.java
@@ -1,15 +1,22 @@
 package com.halowing.lib.web.exception;
 
-public class HttpNotFoundException extends WebApplicationException {
+public class HttpNotFoundException extends SimpleWebApplicationException {
 
 	private static final long serialVersionUID = -4391515543765150039L;
+	
+	public static final int    HTTP_STATUS_NOT_FOUND_ERROR = 404;
+	public static final String HTTP_STATUS_NOT_FOUND_ERROR_CODE = "http.status.not_found_error"; 
+	public static final String HTTP_STATUS_NOT_FOUND_ERROR_MESSAGE = "request uri not founded."; 
 
 	public HttpNotFoundException() {
-		super(404,"request url is not founded.");
+		super(HTTP_STATUS_NOT_FOUND_ERROR,HTTP_STATUS_NOT_FOUND_ERROR_CODE);
 	}
 	
-	public HttpNotFoundException(String message) {
-		super(404, message);
-		
+	public HttpNotFoundException(String args) {
+		super(HTTP_STATUS_NOT_FOUND_ERROR, HTTP_STATUS_NOT_FOUND_ERROR_CODE, args);
+	}
+	
+	public HttpNotFoundException(String errorCode, String args) {
+		super(HTTP_STATUS_NOT_FOUND_ERROR, errorCode, args);
 	}
 }
